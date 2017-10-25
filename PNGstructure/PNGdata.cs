83c39fe12006e7aeef08a5fcadadd49551c5cdcc
l3lackheart <dgbao1340@gmail.com>
2017-10-26 05:21:15 +0700

    Init form desgin

---
+++ PNGstructure/PNGdata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PNGstructure
+{
+    class PNGdata
+    {
+        Control Control;
+
+        byte[] fileData;
+        byte[] ImageData;
+
+    }
+}
